f2064974ce109a327645b2a0e4b527353e6db13b
jsolisifp <jsolis@alumnoifp.es>
2025-06-25 00:11:41 +0200

    Añadido archivo log.cs que había sido excluido por gitignore

---
+++ BaseSystem/Log/Log.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkSimulator
+{
+    public abstract class Log : SimulationSubsystem
+    {
+        public abstract void LogMessage(string category, string message);
+        public abstract void LogWarning(string category, string message);
+        public abstract void LogError(string category, string message);
+    }
+}
